9300c341501ef74d33e33ad51f861e30384e753b
Neehar Patil <pluckynumbat@gmail.com>
2025-07-25 21:04:48 +0530

    added a profile server struct which contains a map of players keyed by their player ids, and a mutex for accessing the player map

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -1,6 +1,11 @@
 // Package profile: service which deals with the player data
 package profile
 
+import (
+	"encoding/json"
+	"sync"
+)
+
 const defaultLevel = 1
 const maxEnergy = 10
 
@@ -8,4 +13,16 @@ type PlayerData struct {
 	PlayerID string `json:"playerID"`
 	Level    int32  `json:"level"`
 	Energy   int32  `json:"energy"`
+}
+
+type Server struct {
+	players      map[string]PlayerData
+	playersMutex sync.Mutex
+}
+
+func NewProfileServer() *Server {
+	return &Server{
+		players:      map[string]PlayerData{},
+		playersMutex: sync.Mutex{},
+	}
 }
