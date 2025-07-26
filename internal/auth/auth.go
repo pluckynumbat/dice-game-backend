3734b1c8612783c33fdf0ea421994a2f1c24e6ac
Neehar Patil <pluckynumbat@gmail.com>
2025-07-26 15:43:01 +0530

    new internal method to generate the player id by taking a sha 256 hash of the username and using the first few elements of it

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -50,3 +50,20 @@ func (as *Server) decodeAuthHeaderPayload(encodedCred string) (string, string, e
 
 	return decodedStrings[0], decodedStrings[1], nil
 }
+
+// generatePlayerID generates a sha 256 hash from the username,
+// and returns the first few elements of it as the new player id
+func (as *Server) generatePlayerID(input string) (string, error) {
+
+	if input == "" {
+		return "", fmt.Errorf("input is empty")
+	}
+
+	hash := sha256.New()
+	hash.Write([]byte(input))
+	hashBytes := hash.Sum(nil)
+
+	resultString := hex.EncodeToString(hashBytes[:4])
+
+	return resultString, nil
+}
