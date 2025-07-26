1279be2ff6b9b65980da449657b42580390dc297
Neehar Patil <pluckynumbat@gmail.com>
2025-07-26 13:27:44 +0530

    add another synced map sessions to the auth server, and create the NewAuthServer function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,3 +1,4 @@
+// Package auth: service which deals with the authenticating the player and managing user sessions
 package auth
 
 import "sync"
@@ -5,4 +6,17 @@ import "sync"
 type Server struct {
 	credentials map[string]string
 	credMutex   sync.Mutex
+
+	sessions map[string]string
+	sessMutex sync.Mutex
+}
+
+func NewAuthServer() *Server {
+	return &Server{
+		credentials: map[string]string{},
+		credMutex:   sync.Mutex{},
+
+		sessions: map[string]string{},
+		sessMutex:   sync.Mutex{},
+	}
 }
