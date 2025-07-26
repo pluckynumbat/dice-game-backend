61f63a5f22969a6cfd72670c8f9d6b3ef41cf63b
Neehar Patil <pluckynumbat@gmail.com>
2025-07-26 15:42:12 +0530

    added an internal method to decode the authorization header payload for basic authentication. reference: https://en.wikipedia.org/wiki/Basic_access_authentication

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -26,3 +26,27 @@ func NewAuthServer() *Server {
 		sessMutex: sync.Mutex{},
 	}
 }
+
+// decodeAuthHeaderPayload will take the authorization header and return a username and password if successful
+// reference: https://en.wikipedia.org/wiki/Basic_access_authentication
+func (as *Server) decodeAuthHeaderPayload(encodedCred string) (string, string, error) {
+
+	if as == nil {
+		return "", "", fmt.Errorf("provided auth server pointer is nil")
+	}
+
+	// trim away the first 6 elements which are the prefix 'Basic '
+	encodedCred = encodedCred[6:]
+
+	// decode the base64 data
+	decodedCred, err := base64.StdEncoding.DecodeString(encodedCred)
+
+	if err != nil {
+		return "", "", fmt.Errorf("cannot decode the given credentials")
+	}
+
+	// separate the username and password
+	decodedStrings := strings.Split(string(decodedCred), ":")
+
+	return decodedStrings[0], decodedStrings[1], nil
+}
