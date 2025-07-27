c23402e91c7ad5cec4ae0125fd2bda81fdf0b1e5
Neehar Patil <pluckynumbat@gmail.com>
2025-07-27 12:40:18 +0530

    rough layout of the handler for an enter level request

--- internal/gameplay/gameplay.go
+++ internal/gameplay/gameplay.go
@@ -16,3 +16,37 @@ type Server struct {
 
 	requestValidator validation.RequestValidator
 }
+
+// HandleEnterLevelRequest accepts / rejects a request to enter a level based on current player data
+func (gs *Server) HandleEnterLevelRequest(w http.ResponseWriter, r *http.Request) {
+
+	if gs == nil {
+		http.Error(w, "provided gameplay server pointer is nil", http.StatusInternalServerError)
+		return
+	}
+
+	err := gs.requestValidator.ValidateRequest(r)
+	if err != nil {
+		w.Header().Set("WWW-Authenticate", "Basic realm=\"User Visible Realm\"")
+		http.Error(w, "session error: "+err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	// TODO: decode the request
+
+	fmt.Printf("request to enter level %v by player id %v \n ")
+
+	// TODO: get the config and the player data
+
+	// TODO: compare level requirements with player data
+
+	// TODO: if player can enter, reduce the amount of energy
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// TODO: send level entry acceptance / rejection in response
+	err = json.NewEncoder(w).Encode(&EnterLevelResponse{})
+	if err != nil {
+		http.Error(w, "could not encode the response", http.StatusInternalServerError)
+	}
+}
