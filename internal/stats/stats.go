e0e89aef7fa4ceab508e0ee64cbdd872b25773e6
Neehar Patil <pluckynumbat@gmail.com>
2025-07-28 11:55:16 +0530

    added a handler for the get stats request, which will return all the stats entries for a given player (if they exist)

--- internal/stats/stats.go
+++ internal/stats/stats.go
@@ -2,7 +2,10 @@
 package stats
 
 import (
+	"encoding/json"
 	"example.com/dice-game-backend/internal/validation"
+	"fmt"
+	"net/http"
 	"sync"
 )
 
@@ -11,7 +14,7 @@ type PlayerLevelStats struct {
 	Level     int32 `json:"level"`
 	WinCount  int32 `json:"winCount"`
 	LossCount int32 `json:"lossCount"`
-	BestScore int32 `json:"BestScore"`
+	BestScore int32 `json:"bestScore"`
 }
 
 // PlayerStats are for all levels for a given player
@@ -31,5 +34,40 @@ func NewStatsServer(rv validation.RequestValidator) *Server {
 		allStats:         map[string]PlayerStats{},
 		statsMutex:       sync.Mutex{},
 		requestValidator: rv,
+	}
+}
+
+// HandlePlayerStatsRequest responds with the player stats data if present
+func (ss *Server) HandlePlayerStatsRequest(w http.ResponseWriter, r *http.Request) {
+
+	if ss == nil {
+		http.Error(w, "provided profile server pointer is nil", http.StatusInternalServerError)
+		return
+	}
+
+	err := ss.requestValidator.ValidateRequest(r)
+	if err != nil {
+		w.Header().Set("WWW-Authenticate", "Basic realm=\"User Visible Realm\"")
+		http.Error(w, "session error: "+err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	id := r.PathValue("id")
+	fmt.Printf("player stats requested for id: %v \n ", id)
+
+	ss.statsMutex.Lock()
+	defer ss.statsMutex.Unlock()
+
+	statsData := &PlayerStats{}
+
+	playerStats, ok := ss.allStats[id]
+	if ok {
+		statsData = &playerStats
+	} // if no stats exist for the player yet, send an empty entry
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(statsData)
+	if err != nil {
+		http.Error(w, "could not encode player data", http.StatusInternalServerError)
 	}
 }
