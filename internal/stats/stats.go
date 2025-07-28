63561e5316dad38af48d8e33314e3b9d2fcd5aff
Neehar Patil <pluckynumbat@gmail.com>
2025-07-28 10:13:16 +0530

    defined the stats server struct which will hold a map keyed on player ids, and have that player's stats as values... new function to create a stats server similar to other contruction functions

--- internal/stats/stats.go
+++ internal/stats/stats.go
@@ -1,6 +1,12 @@
 // Package stats: service which holds and provides details regarding relevant player stats
 package stats
 
+import (
+	"example.com/dice-game-backend/internal/validation"
+	"sync"
+)
+
+// PlayerLevelStats are for a given level for a given player
 type PlayerLevelStats struct {
 	Level     int32 `json:"level"`
 	WinCount  int32 `json:"winCount"`
@@ -8,6 +14,22 @@ type PlayerLevelStats struct {
 	BestScore int32 `json:"BestScore"`
 }
 
+// PlayerStats are for all levels for a given player
 type PlayerStats struct {
 	LevelStats []PlayerLevelStats `json:"levelStats"`
+}
+
+type Server struct {
+	allStats   map[string]PlayerStats // this is for all players (and all levels)
+	statsMutex sync.Mutex
+
+	requestValidator validation.RequestValidator
+}
+
+func NewStatsServer(rv validation.RequestValidator) *Server {
+	return &Server{
+		allStats:         map[string]PlayerStats{},
+		statsMutex:       sync.Mutex{},
+		requestValidator: rv,
+	}
 }
