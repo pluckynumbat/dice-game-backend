f21152dab3703e2bd713eb9a4f083eeb0b087c71
Neehar Patil <pluckynumbat@gmail.com>
2025-07-29 20:20:33 +0530

    added a test function for the UpdatePlayerData() method

--- internal/profile/profile_test.go
+++ internal/profile/profile_test.go
@@ -62,3 +62,46 @@ func TestServer_GetPlayer(t *testing.T) {
 		})
 	}
 }
+
+func TestServer_UpdatePlayerData(t *testing.T) {
+
+	authServer := auth.NewAuthServer()
+	ps := NewProfileServer(authServer, config.NewConfigServer(authServer).GameConfig)
+	ps.players["player2"] = PlayerData{"player2", 1, 20, time.Now().UTC().Unix()}
+	ps.players["player3"] = PlayerData{"player3", 2, 20, time.Now().UTC().Unix()}
+	ps.players["player4"] = PlayerData{"player4", 10, 50, time.Now().UTC().Unix()}
+
+	tests := []struct {
+		name        string
+		server      *Server
+		playerID    string
+		energyDelta int32
+		newLevel    int32
+		wantPlayer  *PlayerData
+		expError    error
+	}{
+		{"nil server", nil, "", 0, 0, nil, serverNilError},
+		{"invalid player", ps, "player1", 0, 0, nil, playerNotFoundErr{"player1"}},
+		{"valid player, more energy", ps, "player2", 20, 1, &PlayerData{"player2", 1, 40, time.Now().UTC().Unix()}, nil},
+		{"valid player, new level", ps, "player3", 10, 3, &PlayerData{"player3", 3, 30, time.Now().UTC().Unix()}, nil},
+		{"valid player, max energy, max level, ", ps, "player4", 100, 100, &PlayerData{"player4", 10, 50, time.Now().UTC().Unix()}, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			gotPlayer, gotErr := test.server.UpdatePlayerData(test.playerID, test.energyDelta, test.newLevel)
+			if gotErr != nil {
+				if errors.Is(gotErr, test.expError) {
+					fmt.Println(gotErr)
+				} else {
+					t.Fatalf("UpdatePlayerData() failed with an unexpected error, %v", gotErr)
+				}
+			} else {
+				if !reflect.DeepEqual(gotPlayer, test.wantPlayer) {
+					t.Errorf("UpdatePlayerData() gave incorrect results, want: %v, got: %v", test.wantPlayer, gotPlayer)
+				}
+			}
+		})
+	}
+}
