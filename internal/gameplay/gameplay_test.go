6d7d20affc7afac64505e6d90a74e87df9870aba
Neehar Patil <pluckynumbat@gmail.com>
2025-07-30 12:58:00 +0530

    adding the usual setup test auth helper function to get the session id needed in other handler tests

--- internal/gameplay/gameplay_test.go
+++ internal/gameplay/gameplay_test.go
@@ -1,10 +1,15 @@
 package gameplay
 
 import (
+	"bytes"
+	"encoding/json"
 	"example.com/dice-game-backend/internal/auth"
 	"example.com/dice-game-backend/internal/config"
 	"example.com/dice-game-backend/internal/profile"
 	"example.com/dice-game-backend/internal/stats"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -20,4 +25,23 @@ func TestNewGameplayServer(t *testing.T) {
 	if gs == nil {
 		t.Fatal("new profile server should not return a nil server pointer")
 	}
+}
+
+func setupTestAuth() (*auth.Server, string, error) {
+	buf := &bytes.Buffer{}
+	reqBody := &auth.LoginRequestBody{IsNewUser: true, ServerVersion: "0"}
+	err := json.NewEncoder(buf).Encode(reqBody)
+	if err != nil {
+		return nil, "", err
+	}
+
+	newAuthReq := httptest.NewRequest(http.MethodPost, "/auth/login", buf)
+	newAuthReq.SetBasicAuth("user1", "pass1")
+	authRespRec := httptest.NewRecorder()
+
+	as := auth.NewAuthServer()
+	as.HandleLoginRequest(authRespRec, newAuthReq)
+	sID := authRespRec.Header().Get("Session-Id")
+
+	return as, sID, nil
 }
