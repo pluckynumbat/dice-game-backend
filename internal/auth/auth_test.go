f41f12326afb501203bff4a2525bde62d8d5fc64
Neehar Patil <pluckynumbat@gmail.com>
2025-07-31 15:51:10 +0530

    added a test function to test the auth service's ValidateRequest() function

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -36,3 +36,47 @@ func TestNewAuthServer(t *testing.T) {
 		t.Error("new profile server's server version should be the current UTC unix timestamp in seconds")
 	}
 }
+
+func TestServer_ValidateRequest(t *testing.T) {
+
+	as := NewAuthServer()
+	as.sessions["testsessionid3"] = &SessionData{
+		PlayerID:       "",
+		SessionID:      "testsessionid3",
+		LastActionTime: 0,
+	}
+
+	newAuthReq := httptest.NewRequest(http.MethodPost, "/test/", nil)
+
+	newAuthReq2 := httptest.NewRequest(http.MethodPost, "/test/", nil)
+	newAuthReq2.Header.Set("Session-Id", "test")
+
+	newAuthReq3 := httptest.NewRequest(http.MethodPost, "/test/", nil)
+	newAuthReq3.Header.Set("Session-Id", "testsessionid3")
+
+	tests := []struct {
+		name        string
+		server      *Server
+		httpRequest *http.Request
+		expError    error
+	}{
+		{"nil server", nil, nil, serverNilError},
+		{"blank session id", as, newAuthReq, missingSessionIDError},
+		{"invalid session", as, newAuthReq2, invalidSessionError},
+		{"valid session", as, newAuthReq3, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			gotErr := test.server.ValidateRequest(test.httpRequest)
+			if gotErr != nil {
+				if errors.Is(gotErr, test.expError) {
+					fmt.Println(gotErr)
+				} else {
+					t.Fatalf("ValidateRequest() failed with an unexpected error, %v", gotErr)
+				}
+			}
+		})
+	}
+}
