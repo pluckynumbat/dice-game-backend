5ed5cc12b4e5f9887c6e044f0b9d181e113d5c4e
Neehar Patil <pluckynumbat@gmail.com>
2025-08-02 17:44:22 +0530

    comment about types package

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,4 +1,5 @@
 // Package types contains types shared by services
+// currently used for data that previously only in profile and stats, but is now used in data service as well
 package types
 
 // NewPlayerRequestBody just contains the player ID
