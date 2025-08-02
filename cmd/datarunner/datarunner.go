a89f4840593a19c5d3fb51773b48a81b7844a397
Neehar Patil <pluckynumbat@gmail.com>
2025-08-02 17:46:29 +0530

    added the data runner go executable command which is used to spin up a data server as an independent microservice on the given port

---
+++ cmd/datarunner/datarunner.go
@@ -0,0 +1,14 @@
+// Used to spin up a data server as an independent microservice on the given port
+package main
+
+import (
+	"example.com/dice-game-backend/internal/constants"
+	"example.com/dice-game-backend/internal/data"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("starting the data server...")
+	dataServer := data.NewDataServer()
+	dataServer.RunDataServer(constants.DataServerPort)
+}
