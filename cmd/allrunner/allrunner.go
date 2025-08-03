c1f03b1c0a725c61bfe0e38fed889c40f9cd8b8b
Neehar Patil <pluckynumbat@gmail.com>
2025-08-03 21:36:52 +0530

    adding a new convenience runner that can be used to start all the micro-services from a single command, and then stop them all together based on user input

---
+++ cmd/allrunner/allrunner.go
@@ -0,0 +1,9 @@
+// Convenience runner used to spin up all the different microservices from a single
+// terminal command, and then wait on user input to shut them all down when done
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("starting all the servers...")
+}
