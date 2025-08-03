a70859825e3c2068c1011be976a95c8bc31bb5c6
Neehar Patil <pluckynumbat@gmail.com>
2025-08-03 12:10:25 +0530

    configrunner: new executable command to spin up a config server as an independent microservice on the designated  port

---
+++ cmd/configrunner/configrunner.go
@@ -0,0 +1,28 @@
+// Used to spin up a config server as an independent microservice on the designated port
+package main
+
+import (
+	"example.com/dice-game-backend/internal/config"
+	"example.com/dice-game-backend/internal/constants"
+	"example.com/dice-game-backend/internal/validation"
+	"fmt"
+	"net/http"
+)
+
+type requestValidator struct {
+}
+
+func (rv *requestValidator) ValidateRequest(req *http.Request) error {
+
+	if rv == nil {
+		return fmt.Errorf("the validator is nil")
+	}
+
+	return validation.ValidateRequest(req)
+}
+
+func main() {
+	fmt.Println("starting the config server...")
+	configServer := config.NewConfigServer(&requestValidator{})
+	configServer.Run(constants.ConfigServerPort)
+}
