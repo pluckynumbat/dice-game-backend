c75ae0b5b8976d7a911114112e9c4e570ac1afdf
Neehar Patil <pluckynumbat@gmail.com>
2025-08-03 14:59:13 +0530

    gameplayrunner: new executable command to spin up a gameplay server as an independent microservice on the given port

---
+++ cmd/gameplayrunner/gameplayrunner.go
@@ -0,0 +1,26 @@
+// Used to spin up a gameplay server as an independent microservice on the given port
+package main
+
+import (
+	"example.com/dice-game-backend/internal/constants"
+	"example.com/dice-game-backend/internal/gameplay"
+	"example.com/dice-game-backend/internal/validation"
+	"fmt"
+	"net/http"
+)
+
+type requestValidator struct{}
+
+func (rv *requestValidator) ValidateRequest(req *http.Request) error {
+
+	if rv == nil {
+		return fmt.Errorf("the validator is nil")
+	}
+	return validation.ValidateRequest(req)
+}
+
+func main() {
+	fmt.Println("starting the gameplay server...")
+	gameplayServer := gameplay.NewGameplayServer(&requestValidator{})
+	gameplayServer.Run(constants.GameplayServerPort)
+}
