3d778a5706a5e74194092714e08f6adc45887311
Neehar Patil <pluckynumbat@gmail.com>
2025-08-03 21:44:43 +0530

    added the standard request validator implementation wrapper to the allrunner

--- cmd/allrunner/allrunner.go
+++ cmd/allrunner/allrunner.go
@@ -2,7 +2,26 @@
 // terminal command, and then wait on user input to shut them all down when done
 package main
 
-import "fmt"
+import (
+	"example.com/dice-game-backend/internal/validation"
+	"fmt"
+	"net/http"
+)
+
+// the request validator struct implements a wrapper around the common method
+// that propagates session based validation requests to the auth service
+type requestValidator struct {
+}
+
+func (rv *requestValidator) ValidateRequest(req *http.Request) error {
+
+	if rv == nil {
+		return fmt.Errorf("the validator is nil")
+	}
+
+	return validation.ValidateRequest(req)
+}
+
 
 func main() {
 	fmt.Println("starting all the servers...")
