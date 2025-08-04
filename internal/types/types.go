cc12d30e6e59eefc36ab3af7897d68af80f19240
Neehar Patil <pluckynumbat@gmail.com>
2025-08-04 11:09:35 +0530

    moved the structs which are used in / by the data storage service to the data package

--- internal/types/types.go
+++ internal/types/types.go
@@ -7,41 +7,12 @@ type NewPlayerRequestBody struct {
 	PlayerID string `json:"playerID"`
 }
 
-// PlayerData (response struct for the client requests and request body for write requests to the data service)
-// stores player related live data like level , energy etc.
-type PlayerData struct {
-	PlayerID       string `json:"playerID"`
-	Level          int32  `json:"level"`
-	Energy         int32  `json:"energy"`
-	LastUpdateTime int64  `json:"lastUpdateTime"`
-}
-
 // PlayerIDLevelEnergy is used as a request body for the internal request to
 // update players data and return them
 type PlayerIDLevelEnergy struct {
 	PlayerID    string `json:"playerID"`
 	Level       int32  `json:"level"`
 	EnergyDelta int32  `json:"energyDelta"`
-}
-
-// PlayerLevelStats are for a given level for a given player
-type PlayerLevelStats struct {
-	Level     int32 `json:"level"`
-	WinCount  int32 `json:"winCount"`
-	LossCount int32 `json:"lossCount"`
-	BestScore int32 `json:"bestScore"`
-}
-
-// PlayerStats are for all levels for a given player
-type PlayerStats struct {
-	LevelStats []PlayerLevelStats `json:"levelStats"`
-}
-
-// PlayerStatsWithID is used as the client response for the public get stats api
-// and as the request body for the internal request to the data service to write stats to the DB
-type PlayerStatsWithID struct {
-	PlayerID    string      `json:"playerID"`
-	PlayerStats PlayerStats `json:"playerStats"`
 }
 
 // PlayerIDLevelStats is used as a request body for the internal request to
